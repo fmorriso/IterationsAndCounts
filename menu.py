059c1ef687bfc5442e2ada04350c2e60b70248db
Frederick Morrison <frederick.morrison@outlook.com>
2023-03-10 10:00:27 -0700

    scaffold Menu class

---
+++ menu.py
@@ -0,0 +1,7 @@
+# Project 3.2.3 - Combo Menu Iteration with Loops
+# Author: Fred Morrison
+# Date written: 2023-03-10
+# Description: A class that encapsulates all aspects of the menu of available food and beverages
+
+class Menu:
+    pass
